7a29bde9b9a649fdef0a6422a42537d7a55aaf07
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-08-26 18:09:44 +0200

    added milestone 2

--- jscript/script.js
+++ jscript/script.js
@@ -1,10 +1,26 @@
 // FUNZIONI
+// funzione che crea una cella con testo interno pari al parametro
+// content
 function createCell (content) {
   const cell = document.createElement('div');
   cell.className = 'cell';
   cell.append(content);
   return cell;
 }
+
+// funzione per generare tot numeri casuali diversi tra loro e che
+// non maggiori di un max
+function getRandomNumbers (max, totalNumbers) {
+  const numbers = [];
+
+  while (numbers.length < totalNumbers) {
+    const randNum = Math.floor(Math.random() * max) + 1;
+    if (!numbers.includes(randNum)) numbers.push(randNum);
+  }
+
+  return numbers;
+}
+
 
 // FASE DI PREPARAZIONE
 // -Prendo la griglia
@@ -26,8 +42,14 @@ const rowsExp = 7;
 const colsExp = 7;
 let totCells;
 
+// imposto il numero di bombe fisso e inizializzo l'array di bombe
+const totalBombs = 16;
+let bombs;
+
 // -Iniazializzo lo score
 let scoreNum = 0;
+
+
 
 // FASE DI ELABORAZIONE
 // -Attacco un event listener sul bottone play
@@ -38,6 +60,7 @@ button.addEventListener('click', function () {
   // -Cambio il testo del button
   if (button.innerText === 'Play' ) button.innerText = 'Play again';
 
+  
   // -Uso uno switch per gestire i tre casi di difficoltà sulla base
   // del valore di selectValue. Creo per ogni caso una tabella di
   // dimensione differente
@@ -46,9 +69,13 @@ button.addEventListener('click', function () {
     case 'lvl-1':
       // Calcolo la variabile totCells
       totCells = rowsBeg * colsBeg;
-
+      
       // -Determino la classe che dovrà essere assegnta alla cella
       cellDimensionClass = 'beginner';
+
+      // -Genero le bombe
+      bombs = getRandomNumbers(totCells, totalBombs);
+      console.log(bombs);
       break;
     case 'lvl-2':
       // -Calcolo la variabile totCells
@@ -56,6 +83,10 @@ button.addEventListener('click', function () {
       
       // -Determino la classe che dovrà essere assegnta alla cella
       cellDimensionClass = 'intermediate';
+      
+      // -Genero le bombe
+      bombs = getRandomNumbers(totCells, totalBombs);
+      console.log(bombs);
       break;
     case 'lvl-3':
       // Calcolo la variabile totCells
@@ -63,35 +94,39 @@ button.addEventListener('click', function () {
       
       // -Determino la classe che dovrà essere assegnta alla cella
       cellDimensionClass = 'expert';
+
+      // -Genero le bombe
+      bombs = getRandomNumbers(totCells, totalBombs);
+      console.log(bombs);
       break;
     default:
-        alert('Seleziona un livello');
-      }
-      // -Inserisco dinamicamente le celle con un ciclo for
-      // e sfrutto la funzione createCell() precedentemente creata
-      for (let i = 0; i < totCells; i++) {
-        const cell = createCell(i + 1);
-        
-        // -Inserisco la classe che dimensiona la cella
-        cell.classList.add(cellDimensionClass);
-        
-        // -Attacco un event listener alla cella che faccia cambiare
-        // il suo colore quando cliccata e che stampi in console il suo
-        // numero
-        cell.addEventListener('click', function () {
-          // -La funzione procede solo se la cella non ha classe 'clicked'
-          if (cell.classList.contains('clicked')) return;
-          // -Aggiungo la classe 'clicked'
-          cell.classList.add('clicked');
-          // -Stampo in pagina il punteggio aumentato di uno
-          score.innerText = ++scoreNum;
-          // -Stampo in console il numero di cella
-          console.log(cell.innerText);
-        })
-        
-        // -Inserisco la cella in pagina
-        grid.appendChild(cell);
-      }
+      alert('Seleziona un livello');
+  }
+  // -Inserisco dinamicamente le celle con un ciclo for
+  // e sfrutto la funzione createCell() precedentemente creata
+  for (let i = 0; i < totCells; i++) {
+    const cell = createCell(i + 1);
+    
+    // -Inserisco la classe che dimensiona la cella
+    cell.classList.add(cellDimensionClass);
+    
+    // -Attacco un event listener alla cella che faccia cambiare
+    // il suo colore quando cliccata e che stampi in console il suo
+    // numero
+    cell.addEventListener('click', function () {
+      // -La funzione procede solo se la cella non ha classe 'clicked'
+      if (cell.classList.contains('clicked')) return;
+      // -Aggiungo la classe 'clicked'
+      cell.classList.add('clicked');
+      // -Stampo in pagina il punteggio aumentato di uno
+      score.innerText = ++scoreNum;
+      // -Stampo in console il numero di cella
+      console.log(cell.innerText);
+    })
+    
+    // -Inserisco la cella in pagina
+    grid.appendChild(cell);
+  }
 });
 
 // -Attacco un event listener alla select per modificare il valore
